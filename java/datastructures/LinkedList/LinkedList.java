6b7dded4a5803a41dadd6bda7cf780718d013747
Cameron Griffin (He/Him) <ct.griffin7@gmail.com>
2023-04-21 19:32:20 -0700

    fix main

--- java/datastructures/LinkedList/LinkedList.java
+++ java/datastructures/LinkedList/LinkedList.java
@@ -1,6 +1,7 @@
 public class LinkedList {
-    private Node head;
+    Node head;
 
+    // Class representing node in the LinkedList
     static class Node {
         private int value;
         private Node next;
@@ -27,16 +28,19 @@ public class LinkedList {
         }
     }
 
+    // LinkedList Constructor
     public LinkedList() {
         head = null;
     }
 
+    // Insert a new node at the head of the LinkedList
     public void insert(int value) {
         Node newNode = new Node(value);
         newNode.setNext(head);
         head = newNode;
     }
 
+    // checks if value exists in the LinkedList
     public boolean includes(int value) {
         Node current = head;
         while (current != null) {
@@ -48,25 +52,27 @@ public class LinkedList {
         return false;
     }
 
+    // return a string representation of the LinkedList
     public String toString() {
         Node current = head;
         StringBuilder output = new StringBuilder();
         while (current != null) {
-            output.append("{").append(current.getValue()).append(" } -> ");
+            output.append("{ ").append(current.getValue()).append(" } -> ");
             current = current.getNext();
         }
         output.append("NULL");
         return output.toString();
     }
 
+    // Main method
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
-        linkedList.insert(1);
-        linkedList.insert(2);
         linkedList.insert(3);
+        linkedList.insert(2);
+        linkedList.insert(1);
 
-        System.out.println(linkedList.includes(2));  // true
-        System.out.println(linkedList.includes(5));  // false
-        System.out.println(linkedList);  // { 3 } -> { 2 } -> { 1 } -> NULL
+        System.out.println(linkedList.includes(2));
+        System.out.println(linkedList.includes(5));
+        System.out.println(linkedList);
     }
 }
